f89cd93f8cb1cfe6fdea8d9a01c84dad8aff3613
kmcknight1 <katiemcknight1991@gmail.com>
2020-04-10 11:48:36 -0500

    completes cityreader stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -83,14 +83,31 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+lat1_lon1 = input("Enter lat1,lon1: ")
+lat2_lon2 = input("Enter lat2,lon2: ")
 
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
-    # within will hold the cities that fall within the specified region
+  # within will hold the cities that fall within the specified region
     within = []
 
     # TODO Ensure that the lat and lon valuse are all floats
     # Go through each city and check to see if it falls within
     # the specified coordinates.
 
+    for city in cities:
+        if city.lat >= min(lat1, lat2) and city.lat <= max(lat1, lat2) and city.lon >= min(lon1, lon2) and city.lon <= max(lon1, lon2):
+            within.append(city)
+
     return within
+
+
+lat1 = float(lat1_lon1.split(",")[0])
+lon1 = float(lat1_lon1.split(",")[1])
+lat2 = float(lat2_lon2.split(",")[0])
+lon2 = float(lat2_lon2.split(",")[1])
+
+cities_within_range = cityreader_stretch(lat1, lon1, lat2, lon2, cities)
+
+for city in cities_within_range:
+    print(city)
